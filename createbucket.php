b41f9aeb59af46e6eb72e24a9bf27e277fbde478
acloudguru <acloudguru@gmail.com>
2015-02-07 23:34:14 +0000

    Create createbucket.php

---
+++ createbucket.php
@@ -0,0 +1,20 @@
+<?php
+//copyright 2015 - A Cloud Guru.
+
+//connection string
+include 'connecttoaws.php';
+
+// Create a unique bucket name
+$bucket = uniqid("acloudguru", true);
+
+// Create our bucket using our unique bucket name
+$result = $client->createBucket(array(
+    'Bucket' => $bucket
+));
+
+//HTML to Create our webpage
+echo "<h1 align=\"center\">Hello Cloud Guru!</h1>";
+echo "<div align = \"center\"><img src=\"https://s3-eu-west-1.amazonaws.com/acloudguru/logo.png\"></img></div>";
+echo "<h2 align=\"center\">You have successfully created a bucket called {$bucket}</h2>";
+echo "<div align=\"center\"><a href=\"createfile.php?bucket=$bucket\">Click Here to Continue</a></div>";
+?>
