c06fd64a71a69344fdac184216a0c1e539249ee7
acloudgurugit <acloudguru@gmail.com>
2015-06-27 12:16:45 +0100

    Update connecttoaws.php

--- connecttoaws.php
+++ connecttoaws.php
@@ -1,6 +1,8 @@
 <?php
 // Include the SDK using the Composer autoloader
 require '/var/www/html/vendor/autoload.php';
-use Aws\S3\S3Client;
-$client = S3Client::factory();
+$client = new Aws\S3\S3Client([
+    'version' => 'latest',
+    'region'  => 'us-east-1'
+]);
 ?>
