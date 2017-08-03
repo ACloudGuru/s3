0a8b7ce9a83a9b7d0ecc25754b7900655cc6074c
Mark <IfTrueElseFalse@gmail.com>
2017-08-03 11:51:19 -0600

    Update createfile.php

--- createfile.php
+++ createfile.php
@@ -27,6 +27,6 @@ $result = $client->putObject(array(
 
 //HTML to create our webpage
 echo "<h2 align=\"center\">File - $key has been successfully uploaded to $bucket</h2>";
-echo "<div align = \"center\"><img src=\"https://acloud.guru/images/logo-small-optimised-with-text.png\"></img></div>";
+echo "<div align = \"center\"><img src=\"https://acloud.guru/images/logo-small-optimised.png\"></img></div>";
 echo "<div align = \"center\"><a href=\"readfile.php?bucket=$bucket&key=$key\">Click Here To Read Your File</a></div>";
 ?>
